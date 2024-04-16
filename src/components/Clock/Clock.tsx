d5dd44e2112dfae1adc8f19b02e05197213eb18b
Shevchenko Andrey <demorest49de@gmail.com>
2024-04-16 16:00:30 +0300

    Changes Clock.tsx

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -7,16 +7,22 @@ export const Clock: React.FC<ClockPropsType> = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalID = setInterval(() => {
             setDate(new Date())
         }, 1000)
+        return () => clearInterval(intervalID)
     }, []);
 
+    const get2DigitsString = (number: number) => {
+        console.log('TICK!!');
+        return number < 10 ? `0${number}` : `${number}`
+    }
+
     return <div>
-        <span>{date.getHours()}</span>
+        <span>{get2DigitsString(date.getHours())}</span>
         :
-        <span>{date.getMinutes()}</span>
+        <span>{get2DigitsString(date.getMinutes())}</span>
         :
-        <span>{date.getSeconds()}</span>
+        <span>{get2DigitsString(date.getSeconds())}</span>
     </div>
 }
