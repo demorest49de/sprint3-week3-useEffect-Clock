b2f3ad24b452ab40a224eeda074e928abd72c53e
Shevchenko Andrey <demorest49de@gmail.com>
2024-04-24 19:04:28 +0300

    Changes UseEffect.stories.tsx

--- src/components/Clock/UseEffect.stories.tsx
+++ src/components/Clock/UseEffect.stories.tsx
@@ -7,27 +7,67 @@ export default {
 
 export const UseEffectSetTimeOut = () => {
 
-    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     console.log(' component render: ')
     ;
 
     useEffect(() => {
-        console.log('useEffect every render');
-    })
-
-    useEffect(() => {
-        console.log('useEffect only first render');
-    }, [])
-
-    useEffect(() => {
         console.log('useEffect every counter increased render');
+        console.log(' use effect : ' + counter)
+        return () => {
+            console.log(' reset effect : ' + counter)
+            ;
+        }
     }, [counter])
 
     return <>
-        Hello, {counter} {fake}
-        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        Hello, {counter}
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 }
+
+export const KeysTrackerDemo = () => {
+
+    const [text, setText] = useState('')
+
+    console.log(' component render: ' + text);
+
+    useEffect(() => {
+        window.addEventListener('keydown', (e)=> {
+            setText((state)=> state + e.key)
+            console.log(e.key);
+        })
+
+    }, [])
+
+    return <>
+        Typed text: {text}
+    </>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
